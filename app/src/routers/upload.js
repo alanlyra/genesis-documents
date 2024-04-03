17bc064a9521d67cd3e65c10ac9daa6d9e924b0e
alanlyra <alanlyra7@gmail.com>
2024-04-03 03:09:19 -0300

    roadmapping document

--- app/src/routers/upload.js
+++ app/src/routers/upload.js
@@ -1,5 +1,6 @@
 // Rotas para verificar se a API está funcionando
 const express = require('express')
+const axios = require('axios');
 const router = new express.Router()
 const Projects = require('../models/Projects')
 const uploadPDF = require('../middleware/uploadPDF')
@@ -9,6 +10,8 @@ router.post('/upload', uploadPDF, async (req, res) => {
     try {
         const document = await Projects.addDocument(req)
         await document.preProcessing()
+
+        await roadmapping(document)
 
         const project = await Projects.findById(req.body._id).populate('bibliometrics.documents');
         if (!project) {
@@ -22,6 +25,18 @@ router.post('/upload', uploadPDF, async (req, res) => {
     }
 });
 
-//chamar o preprocessing que estará dentro de services
+async function roadmapping(document) {
+
+    let response = null;
+    try {
+        console.log("Roadmapping document: " + document._id)
+        response = await axios.get('http://roadmap-service:4102/roadmap/document/' + document._id)
+        console.log("Finished roadmapping document: " + document._id)
+        return response
+    } catch (error) {
+        console.log(error);
+    }
+    return response
+}
 
 module.exports = router
